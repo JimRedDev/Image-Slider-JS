063d12b47dd7abd5b5977b5085988075ed3fb3eb
James Bowler <jimuk07@yahoo.co.uk>
2018-10-22 14:10:37 +0100

    script.js complete

---
+++ script.js
@@ -0,0 +1,53 @@
+// Variable declarations
+let sliderImages = document.querySelectorAll('.slide'),
+    arrowLeft = document.querySelector('#arrow-left'),
+    arrowRight = document.querySelector('#arrow-right'),
+    current = 0;
+
+
+// Clear all images - display none
+function reset() {
+    for (var i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = "none";
+    }
+}
+
+// Initialises the first slide
+function startSlide() {
+    reset();
+    sliderImages[0].style.display = "block";
+}
+
+// Show previous - slide left
+function slideLeft() {
+    reset();
+    sliderImages[current - 1].style.display = "block";
+    current--;
+}
+
+// Show next - slide right
+function slideRight() {
+    reset();
+    sliderImages[current + 1].style.display = "block";
+    current++;
+}
+
+// Left arrow click
+arrowLeft.addEventListener('click', () => {
+    if (current === 0) {
+        current = sliderImages.length;
+    }
+    slideLeft();
+})
+
+// Right arrow click
+arrowRight.addEventListener('click', () => {
+    if (current === sliderImages.length - 1) {
+        current = -1;
+    }
+    slideRight();
+})
+
+startSlide();
+
+
